b55e605787bd42f64d6790731ac9301f710f35ff
Dario Ilić <darioilic13@gmail.com>
2023-09-11 10:53:34 +0200

    Create leap_year_checker.cpp

---
+++ leap_year_checker.cpp
@@ -0,0 +1,10 @@
+int main() {
+  int year;
+  std::cout << "Please enter year: ";
+  std::cin >> year;
+  if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+    std::cout << "It is a leap year\n";
+  }
+  else {
+    std::cout << "It is not a leap year\n";
+  }
